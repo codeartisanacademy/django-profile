0d01bd2cb6b48a9be12e57f727406e2913ae49a2
wahyudi <wahyudi.coding@gmail.com>
2020-06-06 11:20:27 +0700

    init

---
+++ profileapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+# Create your models here.
+class Profile(models.Model):
+    phone = models.CharField(max_length=16)
+    bio = models.TextField()
+    photo = models.ImageField(upload_to='profiles', null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
